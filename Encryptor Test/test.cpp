8c54acfc926932bd6084c04fc3a15e72aa0d5641
Alex Taxiera <alex.taxiera@gmail.com>
2018-04-11 21:22:39 -0400

    update tests

--- Encryptor Test/test.cpp
+++ Encryptor Test/test.cpp
@@ -6,7 +6,7 @@ TEST(Encryptor, EveryOther)
     // function should take a string and return a string
     // file IO can be a separate function
   
-    EXPECT_EQ(encryptEveryOther("hello\nworld") , "ifmmp\nworld");
+    EXPECT_EQ(encryptEveryOther("alex\nkeeps the doctor away") , "mfy\nWOW the SHREK away");
 }
 
 TEST(Encryptor, ZeroAPressRun)
@@ -14,7 +14,7 @@ TEST(Encryptor, ZeroAPressRun)
 	// function should take a string and return a string
 	// file IO can be a separate function
 
-    EXPECT_EQ(encryptZeroAPress("Alex ate an apple"), "mfy!uf!o!qqmf");
+    EXPECT_EQ(encryptZeroAPress("Alex ate an apple"), "lex te n pple");
 }
 
 TEST(Encryptor, ShiftUpTest)
